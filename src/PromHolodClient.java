7ffc69b0212165a18d46a493aeed8c09c351440c
Администратор <@Administrator>
2016-08-01 13:23:16 +0300

    0003 неработают клавишы, пробелема цыкла

--- src/PromHolodClient.java
+++ src/PromHolodClient.java
@@ -29,53 +29,57 @@ class MyFrame extends JFrame{
 }
 class MyPanel extends JPanel{
 	public Image im;
-	Font font=new Font("Arial", Font.BOLD, 20);
-	
-	Rectangle server=new Rectangle(500,50,220,35);
-	ArrayList<Rectangle> rectangle=new ArrayList<Rectangle>();	
-	
-	Thread thread = new Thread(new Connect());	
-	
+	Font font=new Font("Arial", Font.BOLD, 20);	
+	Connect connect=new Connect();
+	String console="";
 			MyPanel(){				
 				addMouseListener( new MyMouse());								
 				//try{im = ImageIO.read(new File("Image/tron.jpg"));}catch(IOException exception){}				
-				for (int i = 0, t = 0; i < 9; i++,t+=50){
-					rectangle.add(new Rectangle(800,10+t,150,50));
-				}
+				connect.start(0);
 			}			
 			public void paintComponent(Graphics g){
 				super.paintComponent(g);
 				setBackground(new Color(150,175,255));
 				//g.drawImage(im,0,0,null);
-				g.setFont(font);
-				g.drawRoundRect(500, 50, 220, 35, 25,25);
-				g.drawString("Связатся с Сервером", 505, 75);				
-				
-				g.setColor(Color.cyan);				
+				//консоль на экране
+				g.setColor(Color.red);
+				g.drawString(console, 10, 10);										
 				//рамка
+				g.setColor(Color.cyan);
 				for (int i = 0, t = 0; i < 9; i++,t+=100){					
 					g.drawRect(800, 100+t, 300, 100);					
-				}//текст в рамке
+				}
+				
+				//текст в рамке
+				g.setFont(font);
 				g.setColor(Color.black);
 				for(int i = 0, ot = 0; i < Colection.mainStr.size(); i++,ot+=100){
-					g.drawString(Colection.mainStr.get(i), 810, 140+ot);
-					
-					
-				}
-					
-				
+					g.drawString(Colection.mainStr.get(i), 810, 140+ot);					
+				}//меню				
+				g.drawRoundRect(500, 50, 220, 35, 25,25);
+				g.drawString("В главное меню", 510, 75);
+				//обновление экрана
 				repaint();
 			}	
 			
 			public class MyMouse extends MouseAdapter  {				
 					
-				public void mousePressed(MouseEvent event){				
-					if(server.contains(event.getPoint())){
-						System.out.println("Потытка соеденится");
-						thread = new Thread(new Connect());
-						thread.start();					
-						
-					}					    				
+				ArrayList<Rectangle> rectangles=new ArrayList<Rectangle>();				
+				
+				public void MyMouse(){
+					rectangles.add(new Rectangle(500,50,220,35));
+					
+					for (int i = 0, t = 0; i < rectangles.size(); i++,t+=100){
+						rectangles.add(new Rectangle(800,100+t,300,100));
+					}
+				}
+				public void mousePressed(MouseEvent event){					
+					for(int i=0;i<rectangles.size();i++){
+						if(rectangles.get(i).contains(event.getPoint())){										
+							console="нажато"+i;connect.start(i);break;
+						}	
+					}
+									    				
 				}
 			}
 }
@@ -86,17 +90,20 @@ class Connect implements Runnable{
 	private PrintWriter out;
 	Socket socket=null;
 	
+	Thread thread;
+	int number;
+	
 	public void run(){
 		try {
 			//socket = new Socket("7.102.42.92", 8080);			
 			socket = new Socket("127.0.0.1", 8080);
 			
 			out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);			
-			out.println("1");
+			out.println(number);
 			
 			oin = new ObjectInputStream(socket.getInputStream());
 			Colection.mainStr=(ArrayList<String>) oin.readObject();
-			System.out.println(Colection.mainStr);System.out.println("Данны получены успешно");
+			
 			
 		} catch (IOException | ClassNotFoundException e1) {e1.printStackTrace();}			         
 		finally {							
@@ -104,10 +111,13 @@ class Connect implements Runnable{
 			catch (IOException e) {	System.err.println("Socket not closed");}
 		}
 	}
-	
-	
+	void start(int number){
+		this.number=number;
+		thread = new Thread(this);					
+		thread.start();			
+	}	
 }
 class Colection{	
 	static ArrayList<String> mainStr=new ArrayList<String>();
-	static ArrayList<String> other=new ArrayList<String>();			
+	static ArrayList<String> other=new ArrayList<String>();	
 }
