117cf4da7212755aa17f7bbd508eb9362e998cbd
Администратор <@Administrator>
2016-09-09 19:00:50 +0300

    3 апгрейд текст

--- src/PromHolodClient.java
+++ src/PromHolodClient.java
@@ -9,6 +9,7 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 
 
+
 public class PromHolodClient {
 		
 	public static void main(String[] args) throws ClassNotFoundException, IOException{		
@@ -19,30 +20,38 @@ public class PromHolodClient {
 	}
 }
 class MyFrame extends JFrame{
-		
+	//JTextField textField = new JTextField("",20);
 	public MyFrame()  throws ClassNotFoundException, IOException{
 		setSize(1280,730);
 		MyPanel panel = new MyPanel();
 		Container pane = getContentPane();
-		pane.add(panel);
+		pane.add(panel);	
+		//panel.add(textField);
+		
 	}
 }
 class MyPanel extends JPanel{
 	public Image im;
 	Font font=new Font("Arial", Font.BOLD, 20);	
 	Connect connect=new Connect();
-	String console="";
-	Rectangle rectangle=new Rectangle(500,50,220,35);//гл.кнопка
+	String console="";	
 	ArrayList<Rectangle> rectangles=new ArrayList<Rectangle>();//кнопки
 	
-			MyPanel(){				
+	String text="";
+	
+			MyPanel(){
+				addKeyListener(new MyKey());
+				setFocusable(true);
 				addMouseListener( new MyMouse());								
 				//try{im = ImageIO.read(new File("Image/tron.jpg"));}catch(IOException exception){}				
-				connect.start(0);				
+				connect.start(8);				
 								
 				for (int i = 0, t = 0; i < 7; i++,t+=100){
 					rectangles.add(new Rectangle(800,100+t,300,100));
 				}
+				rectangles.add(new Rectangle(500,50,220,35));//гл.кнопка
+				rectangles.add(new Rectangle(500, 150, 220, 35));//добавить
+				rectangles.add(new Rectangle(500, 250, 220, 35));//удалить
 			}			
 			public void paintComponent(Graphics g){
 				super.paintComponent(g);  repaint();
@@ -50,41 +59,62 @@ class MyPanel extends JPanel{
 				//g.drawImage(im,0,0,null);
 				//консоль на экране
 				g.setColor(Color.red);
-				g.drawString(console, 10, 10);								
+				g.drawString(console, 10, 10);						
 				//рамка
 				g.setColor(Color.cyan);
 				for (int i = 0, t = 0; i < 9; i++,t+=100){				
 					g.drawRect(800, 100+t, 300, 100);					
-				}
+				}//поле для ввода
+				g.setColor(new Color(255,151,185));
+				g.fillRoundRect(800, 40, 300, 50, 30, 30);
 				
 				//текст в рамке
 				g.setFont(font);
 				g.setColor(Color.black);
+				g.drawString(text, 840, 70);
+				g.fillRect(820, 50, 7, 30);
 				for(int i = 0, ot = 0; i < connect.main.size(); i++,ot+=100){
-					g.drawString(connect.main.get(i), 810, 140+ot);					
+					g.drawString(connect.main.get(i), 810, 140+ot);				
 				}//меню				
-				g.drawRoundRect(500, 50, 220, 35, 25,25);
-				g.drawString("В главное меню", 510, 75);				
+				g.drawRoundRect(500, 50, 220, 35, 25,25);		g.drawString("В главное меню", 510, 75);
+				g.drawRoundRect(500, 150, 220, 35, 25,25);		g.drawString("Добавить", 510, 175);
+				g.drawRoundRect(500, 250, 220, 35, 25,25);		g.drawString("Удалить", 510, 275);
 			}	
 			
 			public class MyMouse extends MouseAdapter  {				
 				
 				public void mousePressed(MouseEvent event){				
 					for(int i=0;i<rectangles.size();i++){
-						console="нажато-"+(i+1);
+						console="нажато-"+(i);
 						if(rectangles.get(i).contains(event.getPoint())){										
-							connect.start((i+1));break;
+							connect.start((i));break;
 						}	
+					}					  				
+				}
+			}
+			private class MyKey implements KeyListener{ 
+				boolean back;
+				
+				public void keyPressed(KeyEvent arg0) {
+					//System.out.println("P="+arg0.getKeyCode());
+					if(arg0.getKeyCode()==8){
+						text=text.substring(0, text.length()-1);
+						back=true;
 					}
-					if(rectangle.contains(event.getPoint())){connect.start(0);}   				
+				}
+				public void keyReleased(KeyEvent arg0) {
+					}		
+				public void keyTyped(KeyEvent arg0) {
+					if(!back)text+=String.valueOf(arg0.getKeyChar());
+					back=false;
 				}
 			}
 }
 class Connect implements Runnable{
 	
 	ObjectInputStream oin;		
-	ObjectOutputStream oos;	
-	private PrintWriter out;
+	ObjectOutputStream oos;
+	
 	Socket socket=null;
 	
 	ArrayList<String> main=new ArrayList<String>();
@@ -103,9 +133,10 @@ class Connect implements Runnable{
 			//socket = new Socket("7.102.42.92", 8080);		
 			socket = new Socket("127.0.0.1", 8080);
 			
-			while(true){			
-				out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);			
-				out.println(number);
+			while(true){				
+				oos = new ObjectOutputStream(socket.getOutputStream());	        	 	
+				oos.writeObject(aparat);				   
+				oos.flush();	
 			
 				oin = new ObjectInputStream(socket.getInputStream());
 				if(!tip)main=(ArrayList<String>) oin.readObject();
@@ -123,10 +154,11 @@ class Connect implements Runnable{
 		while(!whil)Thread.sleep(500);		
 	}
 	void start(int number){		
-		this.number=number;
+		aparat.integer=number;
 		whil=true;
 	}	
 }
-class Aparat{	
-	String main;
+class Aparat implements Serializable{
+	int integer;
+	String srt;	
 }
